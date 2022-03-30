fd909a497f8808a10580f5fc4513b7054184f137
SamBeagles <samuellbeagles@gmail.com>
2022-03-30 10:49:07 +0100

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Samuel Beagles (Samuel-L-Beagles, Sam Beagles) is a programmer, musician, games enthusiast
+          who enjoys a challenge and other such endeavours.</p>
+        <p>
+          (This is a sample website - however there will be another at another address but for now go {' '}
+          <a href="https://soundcloud.com/samuel-l-beagles">here</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+}
